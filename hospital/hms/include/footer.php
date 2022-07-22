8bd0e0ee45887b488fcc65b86ebfeb5c04dabd4e
DipakAec <pauldipakkr@gmail.com>
2022-07-23 02:01:12 +0530

    first commit

---
+++ hospital/hms/include/footer.php
@@ -0,0 +1,10 @@
+<footer>
+				<div class="footer-inner">
+					<div class="pull-left">
+						&copy; <span class="current-year"></span><span class="text-bold text-uppercase"> Arogya Hospital</span>. <span>All rights reserved</span> <br><span>Developed by Dipak Kumar Paul</span>
+					</div>
+					<div class="pull-right">
+						<span class="go-top"><i class="ti-angle-up"></i></span>
+					</div>
+				</div>
+			</footer>
